5804dc812dc666f2f8a66863a0b8afd113377eaf
zachary mines <zmines@wesleyan.edu>
2020-06-24 17:23:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  your_hash = {A => ['Apples','Apricots'], B => ['Bananas'], C => ['Clementines']
+  your_hash = {A => ['Apples','Apricots'], B => ['Bananas'], C => ['Clementines']}
 
   
 end
